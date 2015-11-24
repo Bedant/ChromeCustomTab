e67af1fbf24b4c51b9c7f2eb2eb975d93aed02d3
Bedant <bebi2527@gmail.com>
2015-11-24 23:08:51 +0530

    First Commit

---
+++ app/src/main/java/com/example/bebi2/chromecustomtab/MainActivity.java
@@ -0,0 +1,121 @@
+package com.example.bebi2.chromecustomtab;
+
+import android.app.ActivityOptions;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.customtabs.CustomTabsCallback;
+import android.support.customtabs.CustomTabsClient;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.customtabs.CustomTabsServiceConnection;
+import android.support.customtabs.CustomTabsSession;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity{
+
+    static final String URL = "https://github.com/Bedant";
+    CustomTabsClient mClient;
+    CustomTabsSession mCustomTabsSession;
+
+    Context mContext;
+
+
+
+    private static class NavigationCallback extends CustomTabsCallback {
+        @Override
+        public void onNavigationEvent(int navigationEvent, Bundle extras) {
+            Log.w("TAG", "onNavigationEvent: Code = " + navigationEvent);
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mContext = this;
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder(getSession());
+                builder.setToolbarColor(Color.BLUE).setShowTitle(true);
+                prepareMenuItems(builder);
+                prepareActionButton(builder);
+                builder.setStartAnimations(mContext, R.anim.slide_in_right, R.anim.slide_out_left);
+                builder.setExitAnimations(mContext, R.anim.slide_in_left, R.anim.slide_out_right);
+                builder.setCloseButtonIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_arrow_back));
+                CustomTabsIntent customTabsIntent = builder.build();
+                CustomTabsHelper.addKeepAliveExtra(mContext, customTabsIntent.intent);
+                customTabsIntent.launchUrl(MainActivity.this, Uri.parse(URL));
+            }
+        });
+    }
+
+    private CustomTabsSession getSession() {
+        if (mClient == null) {
+            mCustomTabsSession = null;
+        } else if (mCustomTabsSession == null) {
+            mCustomTabsSession = mClient.newSession(new NavigationCallback());
+        }
+        return mCustomTabsSession;
+    }
+
+    private void prepareMenuItems(CustomTabsIntent.Builder builder) {
+        Intent menuIntent = new Intent();
+        menuIntent.setClass(getApplicationContext(), this.getClass());
+        // Optional animation configuration when the user clicks menu items.
+        Bundle menuBundle = ActivityOptions.makeCustomAnimation(this, android.R.anim.slide_in_left,
+                android.R.anim.slide_out_right).toBundle();
+        PendingIntent pi = PendingIntent.getActivity(getApplicationContext(), 0, menuIntent, 0,
+                menuBundle);
+        builder.addMenuItem("Menu entry 1", pi);
+    }
+
+    private void prepareActionButton(CustomTabsIntent.Builder builder) {
+        // An example intent that sends an email.
+        Intent actionIntent = new Intent(Intent.ACTION_SEND);
+        actionIntent.setType("*/*");
+        actionIntent.putExtra(Intent.EXTRA_EMAIL, "example@example.com");
+        actionIntent.putExtra(Intent.EXTRA_SUBJECT, "example");
+        PendingIntent pi = PendingIntent.getActivity(getApplicationContext(), 0, actionIntent, 0);
+        Bitmap icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
+        builder.setActionButton(icon, "send email", pi);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
